03fd18fbbbaf6ac1811955da6315283d87abd15e
SAIFUL ISLAM (Fahim) <saifulislamfahim.me@gmail.com>
2025-06-16 23:07:20 +0600

    :UPDATE: api url

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -1,12 +1,12 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
-    reducerPath: "api",
-    baseQuery: fetchBaseQuery({
-        baseUrl: "https://api.sgf-sa.com/api/v1"
-    }),
-    endpoints: () => ({})
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://18.142.72.13:5000/api/v1",
+  }),
+  endpoints: () => ({}),
 });
 
-export const imageUrl = "https://api.sgf-sa.com";
+export const imageUrl = "http://18.142.72.13:5000";
 // export const imageUrl = "http://192.168.10.185:5000";
