e6680491ca3dcaedc7963dcaf6778e1ffcc3d1c3
SAIFUL ISLAM (Fahim) <saifulislamfahim.me@gmail.com>
2025-06-16 23:33:21 +0600

    Update config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,34 +1,39 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'i.ibb.co',
-                pathname: '**',
-            },
-            {
-                protocol: 'http',
-                hostname: '64.227.170.194',
-                pathname: '**',
-            },
-            {
-                protocol: 'http',
-                hostname: '165.232.147.36',
-                pathname: '**',
-            },
-            {
-                protocol: 'https',
-                hostname: 'api.sgf-sa.com',
-                pathname: '**',
-            },
-            {
-                protocol: 'http',
-                hostname: '192.168.10.185',
-                pathname: '**',
-            }
-        ]
-    }
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+        pathname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "64.227.170.194",
+        pathname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "165.232.147.36",
+        pathname: "**",
+      },
+      {
+        protocol: "https",
+        hostname: "api.sgf-sa.com",
+        pathname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "192.168.10.185",
+        pathname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "18.142.72.13",
+        pathname: "**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
